1c71687e32e4f68b451261abd2ef043606cf984e
Anyway <22cs3021@rgipt.ac.in>
2023-12-14 10:25:04 +0530

    Added About Us file

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get("/signup", function(req, res){
     res.render("signup");
 });
 app.get("/about", function(req, res){
-    res.sendFile(__dirname + "/about.html");
+    res.render("about",{accountname: accname, accounticon: accicon});
 });
 
 app.post("/login", function(req, res){
